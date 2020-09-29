5dc484cc9339eeb3f7ea16e6e321d94177883dbe
Daniel Sparber <daniel@sparber.io>
2020-09-29 16:38:34 +0200

    Implemented AVwq

---
+++ src/av.cpp
@@ -0,0 +1,41 @@
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/warp.h>
+
+NORI_NAMESPACE_BEGIN
+
+class AvIntegrator : public Integrator {
+public:
+    AvIntegrator(const PropertyList &props) {
+        m_length = props.getFloat("length");
+    }
+
+    Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+        /* Find the surface that is visible in the requested direction */
+        Intersection its;
+        if (!scene->rayIntersect(ray, its)){
+            return Color3f(1.0f);
+        }
+
+        /* Compute average visibility */
+        Normal3f n = its.shFrame.n;
+        Ray3f castedRay(its.p, m_length * Warp().sampleUniformHemisphere(sampler, n));
+
+        if (scene->rayIntersect(castedRay, its)) {
+            return Color3f(0.0f);
+        } else {
+            return Color3f(1.0);
+        }
+    }
+
+    std::string toString() const {
+        return "NormalIntegrator[]";
+    }
+
+private:
+    float m_length;
+
+};
+
+NORI_REGISTER_CLASS(AvIntegrator, "av");
+NORI_NAMESPACE_END
